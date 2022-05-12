ccfee2cf41cf1a47d56f50d7c3e2f8432b2e72a8
Rhaka Novyansyah Dewanto <rhakadewanto26@gmail.com>
2022-05-13 01:52:30 +0700

    configure android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance
+    ..initialize()
+    ..updateRequestConfiguration(
+      RequestConfiguration(testDeviceIds: []),
+    );
+
   runApp(
     GetMaterialApp(
       title: "Application",
