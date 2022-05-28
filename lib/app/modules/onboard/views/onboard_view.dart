9eae49eb93a540bcc72da1abd90b9d0c627878b4
ridwanal24 <ridwanalakhyar1999@gmail.com>
2022-05-28 12:07:07 +0700

    make onBoard Center

--- lib/app/modules/onboard/views/onboard_view.dart
+++ lib/app/modules/onboard/views/onboard_view.dart
@@ -17,72 +17,74 @@ class OnboardView extends GetView<OnboardController> {
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Expanded(
-                child: Image.asset('assets/images/onboard.png'),
-              ),
-              const SizedBox(height: 16),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: colorPrimaryAccent,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Spacer(),
-                      Text(
-                        'Welcome To',
-                        style: textStyle.copyWith(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        'Plus Minus Game',
-                        style: textStyle.copyWith(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const Spacer(),
-                      InkWell(
-                        onTap: () {
-                          if (GetStorage().read('isTermAccepted') == null ||
-                              GetStorage().read('isTermAccepted') == false) {
-                            Get.toNamed(Routes.TERMS);
-                          } else {
-                            Get.toNamed(Routes.HOME);
-                          }
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: colorPrimary,
-                            border: Border.all(
-                              color: Colors.white,
-                              width: 2,
-                            ),
-                          ),
-                          child: const Icon(
-                            Icons.arrow_forward,
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Image.asset('assets/images/onboard.png'),
+                ),
+                const SizedBox(height: 16),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: colorPrimaryAccent,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Spacer(),
+                        Text(
+                          'Welcome To',
+                          style: textStyle.copyWith(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white,
                           ),
                         ),
-                      ),
-                      const Spacer(),
-                    ],
+                        const SizedBox(height: 16),
+                        Text(
+                          'Plus Minus Game',
+                          style: textStyle.copyWith(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Spacer(),
+                        InkWell(
+                          onTap: () {
+                            if (GetStorage().read('isTermAccepted') == null ||
+                                GetStorage().read('isTermAccepted') == false) {
+                              Get.toNamed(Routes.TERMS);
+                            } else {
+                              Get.toNamed(Routes.HOME);
+                            }
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: colorPrimary,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
+                              ),
+                            ),
+                            child: const Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        const Spacer(),
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
