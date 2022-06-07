eacd4747afa0fc7614beef49109f9d06ef965b2c
ridwanal24 <ridwanalakhyar1999@gmail.com>
2022-06-07 13:09:45 +0700

    linked to firebase & add icon launcher

--- lib/app/data/enum/ad.dart
+++ lib/app/data/enum/ad.dart
@@ -3,3 +3,10 @@ class AdUnitId {
   static const String adUnitInterstitial =
       'ca-app-pub-8350728512079415/7544402156';
 }
+
+// Banner Id Test : ca-app-pub-3940256099942544/6300978111
+// Interstitial Id Test : ca-app-pub-3940256099942544/1033173712
+
+
+// Banner Id Real : ca-app-pub-8350728512079415/4543081715
+// Interstitial Id Real : ca-app-pub-8350728512079415/7544402156
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cannacal/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,12 +10,14 @@ import 'app/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  MobileAds.instance.initialize();
   MobileAds.instance
     ..initialize()
     ..updateRequestConfiguration(
       RequestConfiguration(testDeviceIds: ["EFC47B3178617470A972F25D4813480D"]),
     );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await GetStorage.init();
 
   runApp(
